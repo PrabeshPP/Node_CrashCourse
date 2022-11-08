855a0152844ff16ad817e3c3e4d9f87c69f495c1
PrabeshPP <prabesh.bista369@gmail.com>
2022-11-08 10:12:03 -0500

    [learned] about file system

---
+++ path_demo.js
@@ -0,0 +1,10 @@
+const path=require("path");
+
+//Base File name
+console.log(path.basename(__filename));
+
+//const extension name
+console.log(path.extname(__filename));
+
+//directory name
+console.log(path.dirname(__filename));
